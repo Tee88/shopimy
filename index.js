1c7061af6460fd9925d0329bfcc66ab5bd480fa2
tee88 <taha.a.saleh@gmail.com>
2018-12-03 23:01:25 +0100

    Complete error handling middleware

--- index.js
+++ index.js
@@ -32,6 +32,23 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+// app.use((req, res, next) => {
+//   const error = new Error('Not Found');
+//   error.status = 404;
+//   next(error);
+// });
+
+// Error handling
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message || 'Internal Server Error'
+    }
+  });
+});
+
 productRoutes(app);
 
 app.get('/', (req, res) => {
