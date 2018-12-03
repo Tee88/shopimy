48a8c8c06df371e62b104ce224698ed725571326
tee88 <taha.a.saleh@gmail.com>
2018-12-03 23:00:46 +0100

    Complete CORS middleware

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ mongoose.connect(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-requested-with, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 productRoutes(app);
 
 app.get('/', (req, res) => {
