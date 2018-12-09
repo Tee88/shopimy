64aae841b7704dcfe249fa539d3d83bb5c529629
tee88 <taha.a.saleh@gmail.com>
2018-12-09 19:54:11 +0100

    Check shop existence on product creation

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import { productSchema } from '../models/productModel';
+import { shopSchema } from '../models/shopModel';
+
 import uniqueValidator from 'mongoose-unique-validator';
 
 const Product = mongoose.model(
@@ -7,13 +9,27 @@ const Product = mongoose.model(
   productSchema.plugin(uniqueValidator)
 );
 
-export const addNewProduct = (req, res) => {
-  let newProduct = new Product(req.body);
+const Shop = mongoose.model('Shop', shopSchema);
 
-  newProduct
-    .save()
-    .then(product => {
-      res.status(201).json(product);
+export const addNewProduct = (req, res) => {
+  const shopName = req.body.shop;
+  Shop.find()
+    .select('name')
+    .then(shops => {
+      const shopsNames = shops.map(shop => {
+        return shop.name;
+      });
+      if (!shopsNames.includes(shopName)) {
+        res.status(404).json({
+          message: `${shopName} not found. shop must be one of the following list [${shopsNames}]`
+        });
+      } else {
+        let newProduct = new Product(req.body);
+
+        newProduct.save().then(product => {
+          res.status(201).json(product);
+        });
+      }
     })
     .catch(err => {
       res.status(500).json({
@@ -54,7 +70,8 @@ export const getProductById = (req, res) => {
 };
 
 export const updateProduct = (req, res) => {
-  Product.findOneAndUpdate({ _id: req.params.productId }, req.body, {
+  const updates = { ...req.body, updatedAt: Date.now() };
+  Product.findOneAndUpdate({ _id: req.params.productId }, updates, {
     new: true
   })
     .then(updatedProduct => {
