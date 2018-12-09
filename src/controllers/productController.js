7f357989304b29dce6050d22499f6b994c5aa3fe
tee88 <taha.a.saleh@gmail.com>
2018-12-09 17:33:36 +0100

    Refactor product controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -10,13 +10,6 @@ const Product = mongoose.model(
 export const addNewProduct = (req, res) => {
   let newProduct = new Product(req.body);
 
-  // newProduct.save((err, product) => {
-  //   if (err) {
-  //     res.send(err);
-  //   }
-  //   res.status(201).json(product);
-  // });
-
   newProduct
     .save()
     .then(product => {
@@ -24,48 +17,66 @@ export const addNewProduct = (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        error: err
+        error: err.message
       });
     });
 };
 
 export const getProducts = (req, res) => {
-  Product.find({}, (err, products) => {
-    if (err) {
-      res.send(err);
-    }
-    res.status(200).json(products);
-  });
+  Product.find()
+    .select('_id name price createdAt')
+    .exec()
+    .then(products => {
+      res.status(200).json(products);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message
+      });
+    });
 };
 
 export const getProductById = (req, res) => {
-  Product.findById(req.params.productId, (err, products) => {
-    if (err) {
-      res.send(err);
-    }
-    res.status(200).json(products);
-  });
+  const id = mongoose.Types.ObjectId(req.params.productId);
+  Product.findById(id)
+    .then(product => {
+      product
+        ? res.status(200).json(product)
+        : res
+            .status(404)
+            .json({ message: 'No valid entry found for provided ID' });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message
+      });
+    });
 };
 
 export const updateProduct = (req, res) => {
-  Product.findOneAndUpdate(
-    { _id: req.params.productId },
-    req.body,
-    { new: true },
-    (err, product) => {
-      if (err) {
-        res.send(err);
-      }
-      res.status(200).json(product);
-    }
-  );
+  Product.findOneAndUpdate({ _id: req.params.productId }, req.body, {
+    new: true
+  })
+    .then(updatedProduct => {
+      res.status(200).json(updatedProduct);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message
+      });
+    });
 };
 
 export const deleteProduct = (req, res) => {
-  Product.remove({ _id: req.params.productId }, (err, product) => {
-    if (err) {
-      res.send(err);
-    }
-    res.status(200).json({ message: 'Successfully deleted product' });
-  });
+  Product.remove({ _id: req.params.productId })
+    .then(() => {
+      res.status(200).json({
+        message: 'Successfully deleted product'
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message
+      });
+    });
 };
